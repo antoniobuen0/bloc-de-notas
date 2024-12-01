0fd5f61ad189f84fbe9638e6ccab4f48f0254dfa
Anto <bueno@uoc.edu>
2024-12-01 02:23:37 +0100

    MásVisibles

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,8 +1,8 @@
 // List of categories for blog posts
 export const CATEGORIES = [
-	'Category 1',
-	'Category 2',
-	'Category 3',
-	'Category 4',
-	'Category 5'
+	'1.Preguntas',
+	'2.Esbozos',
+	'3.Curiosidad',
+	'4.Ciencias',
+	'5.Personal'
 ] as const
